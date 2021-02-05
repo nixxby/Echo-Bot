c72d589f4c216e6ca39e57e823d4a94e58e23ae3
nixxby <52523591+nixxby@users.noreply.github.com>
2021-02-06 00:16:15 +0530

    added date feature in app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
+from datetime import date
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Wow, You are here!!"
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
+    tod = date.today()
     msg = request.form.get('Body')
+    msg += 'Date received: '+ tod
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    resp.message("Echo: {}".format(msg))
 
     return str(resp)
 
